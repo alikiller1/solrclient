140f6c35dd2d1e61aa3a58452e3f01f91282ed86
liuqinghua <liuqinghua@LIUQINGHUA.pycredit.cn>
2017-05-25 20:05:15 +0800

    Signed-off-by: liuqinghua <liuqinghua@LIUQINGHUA.pycredit.cn>

--- src/main/java/com/liuqh/similarity/MySimilarity.java
+++ src/main/java/com/liuqh/similarity/MySimilarity.java
@@ -5,15 +5,19 @@ import org.apache.lucene.search.similarities.SimilarityBase;
 
 public class MySimilarity extends SimilarityBase {
 
-	@Override
-	protected float score(BasicStats stats, float freq, float docLen) {
-		return 1.0f;
-	}
+	
 
 	@Override
 	public String toString() {
 		return null;
 	}
+
+	@Override
+	protected float score(BasicStats stats, float freq, float docLen) {
+		return 0;
+	}
+	
+	
 
 
 }
