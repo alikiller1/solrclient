eaf245edb90b6722a072f89a69a6d4f2f1b4cf6f
alikiller1 <liu2011321@sina.com>
2017-05-25 23:33:40 +0800

    Signed-off-by: alikiller1 <liu2011321@sina.com>

--- src/main/java/com/liuqh/similarity/MySimilarity.java
+++ src/main/java/com/liuqh/similarity/MySimilarity.java
@@ -9,13 +9,14 @@ public class MySimilarity extends SimilarityBase {
 
 	@Override
 	public String toString() {
+		System.out.println("---->MySimilarity");
 		return null;
 	}
 
 	@Override
 	protected float score(BasicStats stats, float freq, float docLen) {
 		//float avgFieldLength=stats.getAvgFieldLength();
-		return 0f;
+		return 0;
 	}
 	
 	
