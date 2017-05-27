fb0e139861aadbbbe20fbae350975fe4c8ac1e2f
liuqinghua <liuqinghua@LIUQINGHUA.pycredit.cn>
2017-05-27 17:21:42 +0800

    Signed-off-by: liuqinghua <liuqinghua@LIUQINGHUA.pycredit.cn>

--- src/main/java/com/liuqh/similarity/MySimilarity.java
+++ src/main/java/com/liuqh/similarity/MySimilarity.java
@@ -15,9 +15,37 @@ public class MySimilarity extends SimilarityBase {
 
 	@Override
 	protected float score(BasicStats stats, float freq, float docLen) {
-		//float avgFieldLength=stats.getAvgFieldLength();
-		return 0;
+//		getAvgFieldLength-->14.0平均半场字段长，
+//		getBoost-->1.0
+//		getValueForNormalization-->1.0
+//		getDocFreq-->3.0在几个文档出现了
+//		getNumberOfDocuments-->9.0文档个数
+//		getNumberOfFieldTokens-->126.0 所有文档总字段长
+//		getTotalTermFreq-->9.0总共的出现次数
+//		freq-->6.0在这个文档出现了几次
+//		docLen-->28.444445文档的长度
+		System.out.println("getDiscountOverlaps-->"+this.getDiscountOverlaps());
+		float avgFieldLength=stats.getAvgFieldLength();
+		float boost=stats.getBoost();
+		float normalization=stats.getValueForNormalization();
+		float docFreq=stats.getDocFreq();
+		float numberOfDocuments=stats.getNumberOfDocuments();
+		float numberOfFieldTokens=stats.getNumberOfFieldTokens();
+		float totalTermFreq=stats.getTotalTermFreq();
+		System.out.println("AvgFieldLength-->"+avgFieldLength);
+		System.out.println("getBoost-->"+boost);
+		System.out.println("getValueForNormalization-->"+normalization);
+		System.out.println("getDocFreq-->"+docFreq);
+		System.out.println("getNumberOfDocuments-->"+numberOfDocuments);
+		System.out.println("getNumberOfFieldTokens-->"+numberOfFieldTokens);
+		System.out.println("getTotalTermFreq-->"+totalTermFreq);
+		System.out.println("freq-->"+freq);
+		System.out.println("docLen-->"+docLen);
+		//freq/g
+		//freq/docLen
+		return 0f;
 	}
+	
 	public static void main(String[] args) {
 		System.out.println(Math.log(5));
 	}
