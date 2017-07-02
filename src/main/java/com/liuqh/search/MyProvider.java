8391ff9957bb637771035221bc0ed42e6c343b32
alikiller1 <liu2011321@sina.com>
2017-07-02 19:58:48 +0800

    Signed-off-by: alikiller1 <liu2011321@sina.com>

--- src/main/java/com/liuqh/search/MyProvider.java
+++ src/main/java/com/liuqh/search/MyProvider.java
@@ -6,6 +6,7 @@ import java.net.URLDecoder;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.LeafReaderContext;
@@ -14,6 +15,8 @@ import org.apache.solr.common.params.SolrParams;
 import org.eclipse.jetty.util.UrlEncoded;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.ctc.wstx.util.StringUtil;
 
 /**
  * Created by qindongliang on 2016/5/9.
@@ -33,27 +36,65 @@ public class MyProvider extends CustomScoreProvider {
 
     @Override
     public float customScore(int doc, float subQueryScore, float valSrcScore) throws IOException {
+    	long allUseTime;
     	long startTime=System.currentTimeMillis();
-    	LeafReader reader= this.context.reader();
-    	Document  docment= reader.document(doc);
-    	String content1=docment.get("content1");
-    	String queryStr=params.get("queryStr");
-    	log.info("get document use time="+(System.currentTimeMillis()-startTime));
-    	queryStr=URLDecoder.decode(queryStr, "UFT-8");
-    	float score=1;
-    	if(content1.equals(queryStr)){
-    		score=4;
+    	long getDocTime;
+    	String mode=params.get("mode");
+    	if(StringUtils.isNotBlank(mode)&&"1".equals(mode)){
+    		   allUseTime=System.currentTimeMillis()-startTime;
+    		   if(allUseTime>0){
+    	        	log.info("all time="+(System.currentTimeMillis()-startTime));
+    	        }
+    		return subQueryScore*valSrcScore;
+    	}else if(StringUtils.isNotBlank(mode)&&"2".equals(mode)){
+    		LeafReader reader= this.context.reader();
+        	Document  docment= reader.document(doc);
+        	 allUseTime=System.currentTimeMillis()-startTime;
+  		   if(allUseTime>0){
+  	        	log.info("all time="+(System.currentTimeMillis()-startTime));
+  	        }
+        	return subQueryScore*valSrcScore;
+    	}else if(StringUtils.isNotBlank(mode)&&"3".equals(mode)){
+    		LeafReader reader= this.context.reader();
+        	Document  docment= reader.document(doc);
+        	String content1=docment.get("content1");
+        	String name=docment.get("name");
+        	String age=docment.get("age");
+        	allUseTime=System.currentTimeMillis()-startTime;
+  		   if(allUseTime>0){
+  	        	log.info("all time="+(System.currentTimeMillis()-startTime));
+  	        }
+        	return subQueryScore*valSrcScore;
     	}else{
-    		if(content1.startsWith(queryStr)){
-    			score=3;
-    		}else{
-    			score=0;
-    		}
+    		LeafReader reader= this.context.reader();
+        	Document  docment= reader.document(doc);
+        	String content1=docment.get("content1");
+        	String name=docment.get("name");
+        	String age=docment.get("age");
+        	getDocTime=System.currentTimeMillis()-startTime;
+        	if(getDocTime>0){
+        		log.info("get document use time="+getDocTime);
+        	}
+        	String queryStr=params.get("queryStr");
+        	queryStr=URLDecoder.decode(queryStr, "UFT-8");
+        	float score=1;
+        	if(content1.equals(queryStr)){
+        		score=4;
+        	}else{
+        		if(content1.startsWith(queryStr)){
+        			score=3;
+        		}else{
+        			score=0;
+        		}
+        	}
+    		//log.info("查询一次：docid:{} content1:{} queryStr:{}", doc, content1, queryStr);
+             allUseTime=System.currentTimeMillis()-startTime;
+            if(allUseTime>0){
+            	log.info("all time="+(System.currentTimeMillis()-startTime));
+            }
+            return  score;
     	}
-        log.info("查询一次：docid:{} content1:{} queryStr:{}",
-                doc, content1,queryStr);
-        log.info("all time="+(System.currentTimeMillis()-startTime));
-        return  score;
+    	
     }
     public static void main(String[] args) throws UnsupportedEncodingException {
     	String s="深圳+拍拍";
