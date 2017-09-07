406b5bae3d161aa11b548ecc41003b7846cba14b
liuqinghua <liuqinghua@LIUQINGHUA.pycredit.cn>
2017-09-07 15:02:24 +0800

    Signed-off-by: liuqinghua <liuqinghua@LIUQINGHUA.pycredit.cn>

--- src/main/java/com/liuqh/solrclient/PinyinUtils.java
+++ src/main/java/com/liuqh/solrclient/PinyinUtils.java
@@ -4,8 +4,8 @@ import org.nlpcn.commons.lang.pinyin.Pinyin;
 
 public class PinyinUtils {
 	public static void main(String[] args) {
-		System.out.println(Pinyin.pinyin("中国"));
-		System.out.println(Pinyin.tonePinyin("中国"));
+		System.out.println(Pinyin.pinyin("中国我们的慕尼黑"));
+		System.out.println(Pinyin.tonePinyin("中国我们的慕尼黑"));
 		System.out.println(Pinyin.unicodePinyin("中国"));
 	}
 }
